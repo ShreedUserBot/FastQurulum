d174f269ff682b27ff111f5e8f17e05c2aee121d
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-04-15 02:54:41 +0400

    Update __main__.py

--- fast_installer/__main__.py
+++ fast_installer/__main__.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
     config['WARN_LIMIT'] = "3"
     config['WARN_MODE'] = "gmute"
     config['LANGUAGE'] = LANGUAGE
-    config['TELEGRAPH_SHORT_NAME'] = "shreeduserbot"
+    config['TELEGRAPH_SHORT_NAME'] = "fastuserbot"
     config["TMP_DOWNLOAD_DIRECTORY"] = "./DOWNLOADS/"
 
     basarili(LANG['SUCCESS_CONFIG'])
